177f24347f0ff36bd4804e7eabbcd37c8eb4e762
Brendan Ejike <ejike.br@gmail.com>
2021-07-15 18:19:20 +0100

    Add support for selectable text

--- lib/auto_size_text.dart
+++ lib/auto_size_text.dart
@@ -3,6 +3,8 @@ library auto_size_text;
 
 import 'dart:async';
 
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 part 'src/auto_size_text.dart';
